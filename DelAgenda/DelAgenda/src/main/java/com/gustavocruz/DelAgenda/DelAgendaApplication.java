438cf39dd7aeb0efad19a770b75fb4c42ae6986a
GustavoCruzDev <gustavocruz.silva@ucsal.edu.br>
2025-06-25 22:06:11 -0300

    First Commit

---
+++ DelAgenda/DelAgenda/src/main/java/com/gustavocruz/DelAgenda/DelAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.gustavocruz.DelAgenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelAgendaApplication.class, args);
+	}
+
+}
---
+++ DelAgenda/DelAgenda/src/test/java/com/gustavocruz/DelAgenda/DelAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavocruz.DelAgenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
