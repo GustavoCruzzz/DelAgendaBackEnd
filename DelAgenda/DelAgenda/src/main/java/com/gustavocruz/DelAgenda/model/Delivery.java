05de87cbcae9f3edd5137673bbeb04f5acb31bd3
GustavoCruzDev <gustavocruz.silva@ucsal.edu.br>
2025-06-26 02:50:23 -0300

    delivery and item first imp.

--- DelAgenda/DelAgenda/src/main/java/com/gustavocruz/DelAgenda/model/Delivery.java
+++ DelAgenda/DelAgenda/src/main/java/com/gustavocruz/DelAgenda/model/Delivery.java
@@ -2,6 +2,8 @@ package com.gustavocruz.DelAgenda.model;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table
 public class Delivery {
@@ -20,8 +22,57 @@ public class Delivery {
     private String Adress;
 
     @Column
-    private Item Items;
+    @OneToMany(mappedBy = "delivery")
+    private List<Item> Items;
 
     @Column
     private boolean Status;
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getAdress() {
+        return Adress;
+    }
+
+    public void setAdress(String adress) {
+        Adress = adress;
+    }
+
+    public String getClientNumber() {
+        return clientNumber;
+    }
+
+    public void setClientNumber(String clientNumber) {
+        this.clientNumber = clientNumber;
+    }
+
+    public List<Item> getItems() {
+        return Items;
+    }
+
+    public void setItems(List<Item> items) {
+        Items = items;
+    }
+
+    public boolean isStatus() {
+        return Status;
+    }
+
+    public void setStatus(boolean status) {
+        Status = status;
+    }
 }
--- DelAgenda/DelAgenda/src/main/java/com/gustavocruz/DelAgenda/model/Item.java
+++ DelAgenda/DelAgenda/src/main/java/com/gustavocruz/DelAgenda/model/Item.java
@@ -1,22 +1,52 @@
 package com.gustavocruz.DelAgenda.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+@Entity
+@Table
 public class Item {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
 
     @Column
     private String Name;
 
     @Column
-    private boolean Delivered;
+    private boolean Delivered = false;
 
     @Column
     private int Quantity;
+
+    @ManyToOne
+    private Delivery delivery;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public boolean isDelivered() {
+        return Delivered;
+    }
+
+    public void setDelivered(boolean delivered) {
+        Delivered = delivered;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        Quantity = quantity;
+    }
+
+    public Delivery getDelivery() {
+        return delivery;
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+    }
 }
