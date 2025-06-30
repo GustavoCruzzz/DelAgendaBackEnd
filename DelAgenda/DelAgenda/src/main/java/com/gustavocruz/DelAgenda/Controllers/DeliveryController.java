3cb012961a9c9f643095e0933442d09be6c2443c
GustavoCruzDev <gustavocruz.silva@ucsal.edu.br>
2025-06-30 19:46:25 -0300

    Controller and Service implementations

--- DelAgenda/DelAgenda/src/main/java/com/gustavocruz/DelAgenda/Controllers/DeliveryController.java
+++ DelAgenda/DelAgenda/src/main/java/com/gustavocruz/DelAgenda/Controllers/DeliveryController.java
@@ -2,8 +2,12 @@ package com.gustavocruz.DelAgenda.Controllers;
 
 import com.gustavocruz.DelAgenda.Services.DeliveryService;
 import com.gustavocruz.DelAgenda.model.Delivery;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -17,6 +21,26 @@ DeliveryService service;
 @PostMapping()
 public Delivery create(@RequestBody Delivery del){
     return service.create(del);
+}
+
+@GetMapping(value = "/{id}")
+public Delivery findById(@PathVariable(value = "id")UUID id){
+    return service.findById(id);
+}
+
+@GetMapping
+public List<Delivery> findAll(){
+    return service.findAll();
+}
+
+@PutMapping(value = "/{id}")
+public Delivery update(@PathVariable UUID id, @RequestBody Delivery del){
+    return service.update(id, del);
+}
+
+@DeleteMapping(value = "/{id}")
+public void delete(@PathVariable UUID id){
+    service.delete(id);
 }
 
 }
--- DelAgenda/DelAgenda/src/main/java/com/gustavocruz/DelAgenda/Services/DeliveryService.java
+++ DelAgenda/DelAgenda/src/main/java/com/gustavocruz/DelAgenda/Services/DeliveryService.java
@@ -3,7 +3,11 @@ package com.gustavocruz.DelAgenda.Services;
 import com.gustavocruz.DelAgenda.model.Delivery;
 import com.gustavocruz.DelAgenda.repositories.DeliveryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class DeliveryService {
@@ -15,6 +19,31 @@ DeliveryRepository rep;
 public Delivery create(Delivery del){
     return rep.save(del);
 }
+
+public Delivery findById(UUID id){
+    return rep.findById(id).orElseThrow();
+}
+
+public List<Delivery> findAll(){
+    return rep.findAll();
+}
+
+public Delivery update(UUID id, Delivery newDel){
+    Delivery del = rep.findById(id).orElseThrow();
+    del.setStatus(newDel.getStatus());
+    del.setDate(newDel.getDate());
+    del.setItems(newDel.getItems());
+    del.setAddress(newDel.getAddress());
+    del.setClient_number(newDel.getClient_number());
+    del.setClient_name(newDel.getClient_name());
+    return rep.save(del);
+}
+
+public void delete(UUID id){
+    rep.deleteById(id);
+}
+
+
 
 
 }
